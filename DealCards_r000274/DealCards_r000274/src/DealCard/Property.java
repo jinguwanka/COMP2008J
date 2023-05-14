657ba7d08a382d6e91b1cb356d71e97f758c629c
caolangcheng <caolangcheng@gmail.com>
2023-05-14 22:45:02 +0800

    b

--- DealCards_r000274/DealCards_r000274/src/DealCard/Property.java
+++ DealCards_r000274/DealCards_r000274/src/DealCard/Property.java
@@ -2,12 +2,17 @@ package DealCard;
 
 public class Property extends Card {
 
-    public Property(CardColor cardColor, int value, int maxNum) {
-        super(cardColor, value, maxNum);
-    }
+	public Property(CardColor cardColor, int value, int maxNum) {
+		super(cardColor, value, maxNum);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public String imageString() {
+		// TODO Auto-generated method stub
+		return "Properties/"+super.imageString();
+	}
+	
+	
 
-    @Override
-    public String imageString() {
-        return "Properties/" + super.imageString();
-    }
 }
