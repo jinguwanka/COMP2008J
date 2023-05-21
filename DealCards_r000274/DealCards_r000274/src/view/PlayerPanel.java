1587625c8c448ef0f51f9576bb8a6fc3c6ef8c19
caolangcheng <caolangcheng@gmail.com>
2023-05-21 10:03:21 +0800

    y

--- DealCards_r000274/DealCards_r000274/src/view/PlayerPanel.java
+++ DealCards_r000274/DealCards_r000274/src/view/PlayerPanel.java
@@ -12,39 +12,61 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import javax.swing.JScrollPane;
 
+
 public class PlayerPanel extends JPanel {
 
-    private Player player;
-    private HandPanel handPanel;
-    private BankPanel bankPanel = new BankPanel();
+	/**
+	 * Create the panel.
+	 */
+	private Player player;
+	private HandPanel handPanel;
+	private BankPanel bankPanel=new BankPanel();
+	private PropertyPanel propertyPanel=new PropertyPanel();
+	private JLabel infJLabel=new JLabel(" ");
+	
+	public PlayerPanel(Player player) {
+		setMinimumSize(new Dimension(10, 150));
+		setMaximumSize(new Dimension(32767, 150));
+		
+		this.player=player;
+		player.setPlayerPanel(this);
+		handPanel=new HandPanel(player);
+		handPanel.setMinimumSize(new Dimension(200, 10));
+		handPanel.setMaximumSize(new Dimension(32767, 32767));
+		handPanel.setPreferredSize(new Dimension(200, 120));
+		
+		setBorder(new TitledBorder(null, player.getName(), TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+		if (!(player instanceof Cpu)) {
+			Button button=new Button("NextPlayer");
+			add(button);
+			button.addActionListener(e->player.nextPlayer());
+		}
+		
+		add(handPanel);
+		add(bankPanel);
+		add(propertyPanel);
+		add(infJLabel);
+		
+		
+		bankPanel.displayBankCards(player.getBank().getCards());
+		handPanel.displayActionCards(player.getPlayPile().getActionCards());
+		propertyPanel.displayProperty(player.propertyCards());
+		
 
-    public PlayerPanel(Player player) {
-        this.player = player;
-        initializePanel();
-        displayBankCards();
-        displayActionCards();
-    }
+	}
+	
+	public JLabel getInfJLabel() {
+		return infJLabel;
+	}
+	
+	public void display() {
+		bankPanel.displayBankCards(player.getBank().getCards());
+		handPanel.displayActionCards(player.getPlayPile().getActionCards());
+		propertyPanel.displayProperty(player.propertyCards());
+		repaint();
+	}
+	
+	
 
-    private void initializePanel() {
-        setBorder(new TitledBorder(null, player.getName(), TitledBorder.LEADING, TitledBorder.TOP, null, null));
-        setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
-
-        handPanel = new HandPanel(player);
-        add(handPanel);
-        add(bankPanel);
-    }
-
-    private void displayBankCards() {
-        bankPanel.displayBankCards(player.getBank().getCards());
-    }
-
-    private void displayActionCards() {
-        handPanel.displayActionCards(player.getPlayPile().getActionCards());
-    }
-    public void display() {
-        bankPanel.displayBankCards(player.getBank().getCards());
-        handPanel.displayActionCards(player.getPlayPile().getActionCards());
-        propertyPanel.displayProperty(player.propertyCards());
-        repaint();
-    }
 }
