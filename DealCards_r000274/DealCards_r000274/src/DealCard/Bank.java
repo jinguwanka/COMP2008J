b852ecd004ccf0ea1aaf9dee03fb5ea321602473
caolangcheng <caolangcheng@gmail.com>
2023-05-30 10:07:06 +0800

    修改

--- DealCards_r000274/DealCards_r000274/src/DealCard/Bank.java
+++ DealCards_r000274/DealCards_r000274/src/DealCard/Bank.java
@@ -8,15 +8,6 @@ import java.util.PriorityQueue;
 public class Bank {
     private PriorityQueue<Card> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(Card::getValue));
 
-    public void add(Card card) {
-        if (isSupportedCard(card)) { // Check if the card is supported (MoneyCard, ActionCard, or RentCard)
-            priorityQueue.add(card); // Add the card to the priority queue
-        }
-    }
-
-    private boolean isSupportedCard(Card card) {
-        return card instanceof MoneyCard || card instanceof ActionCard || card instanceof RentCard;
-    }
 
     public List<Card> getCards() {
         return new ArrayList<>(priorityQueue); // Return a new ArrayList containing all the cards in the priority queue
@@ -24,21 +15,37 @@ public class Bank {
 
     public void addAll(List<Card> cards) {
         for (Card card : cards) {
-            add(card); // Add each card from the list to the priority queue
+            add(card);
+        }
+    }
+
+    public void add(Card card) {
+        if (card instanceof MoneyCard||card instanceof ActionCard||card instanceof RentCard) {
+            priorityQueue.add(card);
         }
     }
 
     public List<Card> getMoney(int money) {
         int total = 0;
         List<Card> cards = new ArrayList<>();
-        while (!priorityQueue.isEmpty()) {
-            Card card = priorityQueue.poll(); // Retrieve and remove the card with the lowest value from the priority queue
-            total += card.getValue(); // Increment the total value with the card's value
-            cards.add(card); // Add the card to the result list
+
+        PriorityQueue<Card> minHeap = new PriorityQueue<>(priorityQueue);
+
+        int cardsTotalValue = 0;
+        for (Card card : minHeap) {
+            cardsTotalValue += card.getValue();
+        }
+
+        while (!minHeap.isEmpty()) {
+            Card card = minHeap.poll();
+            total += card.getValue();
+            cards.add(card);
+
             if (total >= money) {
-                break; // Exit the loop if the total value exceeds or equals the required amount of money
+                break;
             }
         }
-        return cards; // Return the list of cards that fulfill the money requirement
+        return new ArrayList<>(cards);
     }
+
 }
