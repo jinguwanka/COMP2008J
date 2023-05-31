e62af817ea4828bab2b7bd35c03287d65b451377
caolangcheng <caolangcheng@gmail.com>
2023-05-31 23:12:02 +0800

    f

--- DealCards_r000274/DealCards_r000274/src/DealCard/CardFactory.java
+++ DealCards_r000274/DealCards_r000274/src/DealCard/CardFactory.java
@@ -3,88 +3,194 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CardFactory {
+	
+	public static List<Card> createCards(String type) {
+		ArrayList<Card> cards=new ArrayList<>();
+		switch (type.toLowerCase()) {
+		case "property":
+			cards.addAll(propertyCards());
+			break;
+		case "wildcard":
+			cards.addAll(wildCards());
+			break;
+		case "actioncard":
+			cards.addAll(actionCards());
+			break;
+		case "rentcard":
+			cards.addAll(rentCards());
+			break;
+		case "moneycard":
+			cards.addAll(moneyCards());
+			break;
 
+		default:
+			break;
+		}
+		return cards;
+	}
+	
+	private static List<Card> moneyCards() {
+		List<Card> cards=new ArrayList<>();
+		for (int i = 0; i < 6; i++) {
+			cards.add(moneyCard(1, 6));
+		}
+		for (int i = 0; i < 5; i++) {
+			cards.add(moneyCard(2, 5));
+		}
+		for (int i = 0; i < 3; i++) {
+			cards.add(moneyCard(3, 3));
+		}
+		for (int i = 0; i < 3; i++) {
+			cards.add(moneyCard(4, 3));
+		}
+		cards.add(moneyCard(5, 2));
+		cards.add(moneyCard(5, 2));
+		cards.add(moneyCard(10, 1));
+		return cards;
+	}
+	
+	private static Card moneyCard(int value,int maxNum) {
+		return new MoneyCard(value, maxNum);
+	}
+	
+	private static List<Card> rentCards() {
+		List<Card> cards=new ArrayList<>();
+		cards.add(rentCard(CardColor.DarkBlue, CardColor.Green, 1, 2));
+		cards.add(rentCard(CardColor.DarkBlue, CardColor.Green, 1, 2));
+		cards.add(rentCard(CardColor.Red, CardColor.Yellow, 1, 2));
+		cards.add(rentCard(CardColor.Red, CardColor.Yellow, 1, 2));
+		
+		cards.add(rentCard(CardColor.Pink, CardColor.Orange, 1, 2));
+		cards.add(rentCard(CardColor.Pink, CardColor.Orange, 1, 2));
+		cards.add(rentCard(CardColor.LightBlue, CardColor.Brown, 1, 2));
+		cards.add(rentCard(CardColor.LightBlue, CardColor.Brown, 1, 2));
+		
+		cards.add(rentCard(CardColor.Railroad, CardColor.Utility, 1, 2));
+		cards.add(rentCard(CardColor.Railroad, CardColor.Utility, 1, 2));
+		cards.add(rentCard(CardColor.MultiColor, CardColor.MultiColor, 3, 3));
+		cards.add(rentCard(CardColor.MultiColor, CardColor.MultiColor, 3, 3));
+		cards.add(rentCard(CardColor.MultiColor, CardColor.MultiColor, 3, 3));
+		return cards;
+		
+	}
+	
+	private static Card rentCard(CardColor cardColor,CardColor secondCardColor,int value,int maxNum) {
+		return new RentCard(cardColor, secondCardColor, value, maxNum);
+	}
+	
+	private static List<Card> actionCards() {
+		List<Card> cards=new ArrayList<>();
+		cards.add(actionCard("dealbreaker", 5, 2));
+		cards.add(actionCard("dealbreaker", 5, 2));
+		cards.add(actionCard("justsayno", 4, 3));
+		cards.add(actionCard("justsayno", 4, 3));
+		cards.add(actionCard("justsayno", 4, 3));
+		
+		cards.add(actionCard("slydeal", 3, 3));
+		cards.add(actionCard("slydeal", 3, 3));
+		cards.add(actionCard("slydeal", 3, 3));
+		
+		cards.add(actionCard("forcedeal", 3, 4));
+		cards.add(actionCard("forcedeal", 3, 4));
+		cards.add(actionCard("forcedeal", 3, 4));
+		cards.add(actionCard("forcedeal", 3, 4));
+		
+		cards.add(actionCard("debtcollector", 3, 3));
+		cards.add(actionCard("debtcollector", 3, 3));
+		cards.add(actionCard("debtcollector", 3, 3));
+		
+		cards.add(actionCard("mybirthday", 2, 3));
+		cards.add(actionCard("mybirthday", 2, 3));
+		cards.add(actionCard("mybirthday", 2, 3));
+		
+		for (int i = 0; i <10; i++) {
+			cards.add(actionCard("passgo", 1, 10));
+		}
+		
+		cards.add(actionCard("house", 3, 3));
+		cards.add(actionCard("house", 3, 3));
+		cards.add(actionCard("house", 3, 3));
+		
+		cards.add(actionCard("hotel", 4, 3));
+		cards.add(actionCard("hotel", 4, 3));
+		cards.add(actionCard("hotel", 4, 3));
+		
+		cards.add(actionCard("doubletherent", 1, 2));
+		cards.add(actionCard("doubletherent", 1, 2));
+		
+		return cards;
+		
+	}
+	
+	private static Card actionCard(String name,int value,int maxNum) {
+		return new  ActionCard(name, value, maxNum);
+	}
+	
+	
+	
+	private static List<Card> wildCards() {
+		List<Card> cards=new ArrayList<>();
+		cards.add(wildCard(CardColor.DarkBlue, CardColor.Green, 4, 1));
+		cards.add(wildCard(CardColor.Green, CardColor.Railroad, 4, 1));
+		cards.add(wildCard(CardColor.Utility, CardColor.Railroad, 2, 1));
+		cards.add(wildCard(CardColor.LightBlue, CardColor.Railroad, 4, 1));
+		cards.add(wildCard(CardColor.LightBlue, CardColor.Brown, 1, 1));
+		cards.add(wildCard(CardColor.Pink, CardColor.Orange, 2, 2));
+		cards.add(wildCard(CardColor.Pink, CardColor.Orange, 2, 2));
+		cards.add(wildCard(CardColor.Red, CardColor.Yellow, 3, 2));
+		cards.add(wildCard(CardColor.MultiColor, CardColor.MultiColor, 0, 2));
+		cards.add(wildCard(CardColor.MultiColor, CardColor.MultiColor, 0, 2));
+		return cards;
+		
+	}
+	
+	private static Card wildCard(CardColor cardColor,CardColor secondCardColor,int value,int maxNum) {
+		return new Wildcard(cardColor, secondCardColor, value, maxNum);
+	}
+	
+	private static List<Card> propertyCards() {
+		List<Card> cards=new ArrayList<>();
+		cards.add(propertyCard(CardColor.DarkBlue, 4, 2));
+		cards.add(propertyCard(CardColor.DarkBlue, 4, 2));
+		cards.add(propertyCard(CardColor.Brown, 1, 2));
+		cards.add(propertyCard(CardColor.Brown, 1, 2));
+		cards.add(propertyCard(CardColor.Utility, 2, 2));
+		cards.add(propertyCard(CardColor.Utility, 2, 2));
+		cards.add(propertyCard(CardColor.Green, 4, 3));
+		cards.add(propertyCard(CardColor.Green, 4, 3));
+		cards.add(propertyCard(CardColor.Green, 4, 3));
+		
+		cards.add(propertyCard(CardColor.Yellow, 3, 3));
+		cards.add(propertyCard(CardColor.Yellow, 3, 3));
+		cards.add(propertyCard(CardColor.Yellow, 3, 3));
+		
+		cards.add(propertyCard(CardColor.Red, 3, 3));
+		cards.add(propertyCard(CardColor.Red, 3, 3));
+		cards.add(propertyCard(CardColor.Red, 3, 3));
+		
+		cards.add(propertyCard(CardColor.Orange, 2, 3));
+		cards.add(propertyCard(CardColor.Orange, 2, 3));
+		cards.add(propertyCard(CardColor.Orange, 2, 3));
+		
+		cards.add(propertyCard(CardColor.Pink, 2, 3));
+		cards.add(propertyCard(CardColor.Pink, 2, 3));
+		cards.add(propertyCard(CardColor.Pink, 2, 3));
+		
+		cards.add(propertyCard(CardColor.LightBlue, 1, 3));
+		cards.add(propertyCard(CardColor.LightBlue, 1, 3));
+		cards.add(propertyCard(CardColor.LightBlue, 1, 3));
+		
+		cards.add(propertyCard(CardColor.Railroad, 2, 4));
+		cards.add(propertyCard(CardColor.Railroad, 2, 4));
+		cards.add(propertyCard(CardColor.Railroad, 2, 4));
+		cards.add(propertyCard(CardColor.Railroad, 2, 4));
+		
+		return cards;
+		
+	}
+	
+	private static Card propertyCard(CardColor cardColor,int value,int maxNum) {
+		return new Property(cardColor, value, maxNum);
+	}
 
-    public static List<Card> createCards(String type) {
-        ArrayList<Card> cards = new ArrayList<>();
-        switch (type.toLowerCase()) {
-            case "property":
-                cards.addAll(propertyCards());
-                break;
-            case "wildcard":
-                cards.addAll(wildCards());
-                break;
-            case "actioncard":
-                cards.addAll(actionCards());
-                break;
-            case "rentcard":
-                cards.addAll(rentCards());
-                break;
-            case "moneycard":
-                cards.addAll(moneyCards());
-                break;
-            default:
-                break;
-        }
-        return cards;
-    }
-
-    // Create a list of money cards
-    private static List<Card> moneyCards() {
-        List<Card> cards = new ArrayList<>();
-
-        for (int i = 0; i < 6; i++) {
-            cards.add(moneyCard(1, 6));
-        }
-        for (int i = 0; i < 5; i++) {
-            cards.add(moneyCard(2, 5));
-        }
-
-        return cards;
-    }
-
-    private static Card moneyCard(int value, int maxNum) {
-        return new MoneyCard(value, maxNum);
-    }
-
-    // Create a list of rent cards
-    private static List<Card> rentCards() {
-        List<Card> cards = new ArrayList<>();
-        cards.add(rentCard(CardColor.DarkBlue, CardColor.Green, 1, 2));
-        cards.add(rentCard(CardColor.DarkBlue, CardColor.Green, 1, 2));
-        cards.add(rentCard(CardColor.Red, CardColor.Yellow, 1, 2));
-        cards.add(rentCard(CardColor.Red, CardColor.Yellow, 1, 2));
-        return cards;
-    }
-
-
-    private static Card rentCard(CardColor cardColor, CardColor secondCardColor, int value, int maxNum) {
-        return new RentCard(cardColor, secondCardColor, value, maxNum);
-    }
-
-
-    private static List<Card> actionCards() {
-        List<Card> cards = new ArrayList<>();
-
-        return cards;
-    }
-
-    private static Card actionCard(String name, int value, int maxNum) {
-        return new ActionCard(name, value, maxNum);
-    }
-
-
-    private static List<Card> wildCards() {
-        List<Card> cards = new ArrayList<>();
-
-        return cards;
-    }
-
-
-    private static List<Card> propertyCards() {
-        List<Card> cards = new ArrayList<>();
-
-        return cards;
-
-    }
 }
--- DealCards_r000274/DealCards_r000274/src/view/PropertyPanel.java
+++ DealCards_r000274/DealCards_r000274/src/view/PropertyPanel.java
@@ -6,30 +6,29 @@ import DealCard.Card;
 import DealCard.PropertyCards;
 
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.util.List;
 
 public class PropertyPanel extends JPanel {
 
-    /**
-     * Create the panel.
-     */
-    public PropertyPanel() {
-        setPreferredSize(new Dimension(400, 120));
-        setLayout(new FlowLayout(FlowLayout.LEFT));
-    }
+	/**
+	 * Create the panel.
+	 */
+	public PropertyPanel() {
+		setPreferredSize(new Dimension(400, 120));
+		setLayout(null);
 
-    public void displayProperty(List<PropertyCards> propertyCards) {
-        removeAll();
+	}
+	
+	public void displayProperty(List<PropertyCards> propertyCards) {
+		for (int i = 0; i < propertyCards.size(); i++) {
+			PropertyCards propertyCards2=propertyCards.get(i);
+			List<Card> cards=propertyCards2.getCards();
+			for (int j = 0; j < cards.size(); j++) {
+				ImagePanel imagePanel=new ImagePanel(cards.get(j).imageString());
+				imagePanel.setLocation(i*100, j*30);
+				add(imagePanel, 0);
+			}
+		}
+	}
 
-        for (PropertyCards property : propertyCards) {
-            List<Card> cards = property.getCards();
-            for (Card card : cards) {
-                ImagePanel imagePanel = new ImagePanel(card.getImageString());
-                add(imagePanel);
-            }
-        }
-
-
-    }
 }
