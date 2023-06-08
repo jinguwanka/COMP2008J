545b7c8ff96f5d0099cfb563ac843a92f0585e64
jinguwanka <jinguwanka@126.com>
2023-06-08 19:42:44 +0800

    m

--- DealCards_r000274/DealCards_r000274/src/DealCard/Cpu.java
+++ DealCards_r000274/DealCards_r000274/src/DealCard/Cpu.java
@@ -5,36 +5,37 @@ import java.util.Random;
 
 public class Cpu extends Player {
 
-    public Cpu(String name) {
-        super(name);
-        // TODO Auto-generated constructor stub
-    }
+	public Cpu(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		getCardsFromDrawpile();
+		if (Math.random()<0.5) {
+			nextPlayer();
+		} else {
+			List<Card> cards=playPile.getActionCards();
+			Random random=new Random();
+			Card card=cards.get(random.nextInt(cards.size()));
+			dealCardGame.getDrawpile().addRubbish(card);
+			action(card);
+			playPile.remove(card);
+			
+			nextPlayer();
 
-    @Override
-    public void action() {
-        try {
-            Thread.sleep(1000); // Pause execution for 1 second to simulate CPU's turn delay
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        getCardsFromDrawpile();
-        if (Math.random() < 0.5) { // 50% chance to skip turn and pass to the next player
-            nextPlayer();
-        } else {
-            List<Card> cards=playPile.getActionCards();
-            Random random=new Random();
-            Card card=cards.get(random.nextInt(cards.size()));
-            dealCardGame.getDrawpile().addRubbish(card);
-            action(card);
-            playPile.remove(card);
-
-            nextPlayer();
-
-        }
-        displayCards();
-    }
-
-
+		}
+		displayCards();
+	}
+	
+	
 
 }
--- DealCards_r000274/DealCards_r000274/src/view/ImagePanel.java
+++ DealCards_r000274/DealCards_r000274/src/view/ImagePanel.java
@@ -16,61 +16,80 @@ import java.awt.event.MouseEvent;
 import javax.swing.JMenuItem;
 
 public class ImagePanel extends JPanel {
+	
+	private Image image;
 
-    private Image image;
+	/**
+	 * Create the panel.
+	 */
+	private JPopupMenu popupMenu = new JPopupMenu();
+	private JMenuItem mntmNewMenuItem = new JMenuItem("DealCard");
+	public ImagePanel(String imageString) {
+		setSize(80, 120);
+		
+		
+		
+		
+		
+		popupMenu.add(mntmNewMenuItem);
+		
+		if (imageString==null) {
+			return;
+		}
+		
+		try {
+			image=ImageIO.read(new File("images/"+imageString));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("images/"+imageString);
+			e.printStackTrace();
+		}
+		//setSize(80, 120);
 
-    /**
-     * Create the panel.
-     */
-    public ImagePanel(String imageString) {
-        loadImage(imageString);
-    }
+	}
+	
+	public void displayPopMenu() {
+		addPopup(this, popupMenu);
+	}
+	
+	public void addMenuAction(ActionListener actionListener) {
+		mntmNewMenuItem.addActionListener(actionListener);
+	}
+	
+	public void setImageString(String imageString) {
+		try {
+			image=ImageIO.read(new File("images/"+imageString));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paintComponent(g);
+		if (image==null) {
+			return;
+		}
+		g.drawImage(image, 0, 0, 80, 120, this);
+	}
 
-    private void loadImage(String imageString) {
-        if (imageString == null) {
-            return;
-        }
-
-        try {
-            image = ImageIO.read(new File("images/" + imageString));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void setImageString(String imageString) {
-        loadImage(imageString);
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        if (image == null) {
-            return;
-        }
-        g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
-    }
-
-    @Override
-    public Dimension getPreferredSize() {
-        return new Dimension(80, 120);
-    }
-
-    private static void addPopup(Component component, final JPopupMenu popup) {
-        component.addMouseListener(new MouseAdapter() {
-            public void mousePressed(MouseEvent e) {
-                if (e.isPopupTrigger()) {
-                    showMenu(e);
-                }
-            }
-            public void mouseReleased(MouseEvent e) {
-                if (e.isPopupTrigger()) {
-                    showMenu(e);
-                }
-            }
-            private void showMenu(MouseEvent e) {
-                popup.show(e.getComponent(), e.getX(), e.getY());
-            }
-        });
-    }
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
 }
