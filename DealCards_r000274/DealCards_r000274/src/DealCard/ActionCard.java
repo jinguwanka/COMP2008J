4833139d262aa916cfd5e3ffdb736e62f6b5ebf2
caolangcheng <caolangcheng@gmail.com>
2023-06-14 18:53:12 +0800

    GUI修改

--- DealCards_r000274/DealCards_r000274/src/DealCard/ActionCard.java
+++ DealCards_r000274/DealCards_r000274/src/DealCard/ActionCard.java
@@ -5,7 +5,7 @@ public class ActionCard extends Card{
 	private String name;
 	public ActionCard(String name, int value, int maxNum) {
 		super(null, value, maxNum);
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor stub.
 		this.name=name;
 	}
 	
@@ -15,7 +15,7 @@ public class ActionCard extends Card{
 	
 	@Override
 	public String imageString() {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub.
 		return "ActionCards/"+name+".jpg";
 	}
 
--- DealCards_r000274/DealCards_r000274/src/view/Gui.java
+++ DealCards_r000274/DealCards_r000274/src/view/Gui.java
@@ -1,14 +1,19 @@
 package view;
 import java.awt.EventQueue;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import DealCard.Controller;
+import DealCard.Cpu;
+import DealCard.Observer;
 import DealCard.Player;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Dimension;
 import javax.swing.BoxLayout;
 
@@ -18,6 +23,7 @@ public class Gui extends JFrame {
 	private Controller controller=new Controller();
 	private JPanel panelMain;
 	private ImagePanel rubbishPanel=new ImagePanel(null);
+	private List<PlayerPanel> playerPanels=new ArrayList<>();
 	
 
 	/**
@@ -41,7 +47,7 @@ public class Gui extends JFrame {
 	 */
 	public Gui() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 687, 474);
+		setBounds(100, 100, 1118, 704);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -59,17 +65,33 @@ public class Gui extends JFrame {
 		panelMain = new JPanel();
 		contentPane.add(panelMain, BorderLayout.CENTER);
 		panelMain.setLayout(new BoxLayout(panelMain, BoxLayout.Y_AXIS));
-		
 		controller.setGui(this);
+		
 	}
+	
+	private Observer observer=new Observer();
 	
 	public void addPlayer(Player player) {
 		PlayerPanel panel=new PlayerPanel(player);
+		observer.add(player, panel.getInfJLabel());
+		player.setObserver(observer);
 		panelMain.add(panel);
+		playerPanels.add(panel);
+	}
+	
+	public void displayPlayerPanels() {
+		for (PlayerPanel playerPanel : playerPanels) {
+			playerPanel.display();
+			playerPanel.repaint();
+		}
 	}
 	
 	public void setRubbishImage(String imageString) {
+		if (imageString==null) {
+			return;
+		}
 		rubbishPanel.setImageString(imageString);
+		rubbishPanel.repaint();
 	}
 
 }
