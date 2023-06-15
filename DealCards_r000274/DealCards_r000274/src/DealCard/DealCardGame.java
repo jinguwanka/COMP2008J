992ad429f5b5dee48a2e5de7ea70b30acf8dd8a3
hahfe <szf_021106@163.com>
2023-06-15 17:30:50 +0800

    DealCardGame.java,MoneyCard.java

--- DealCards_r000274/DealCards_r000274/src/DealCard/DealCardGame.java
+++ DealCards_r000274/DealCards_r000274/src/DealCard/DealCardGame.java
@@ -4,58 +4,54 @@ import java.util.List;
 import java.util.Scanner;
 
 public class DealCardGame {
-	
+
 	private int playerNum;
-	private List<Player> players=new ArrayList<>();
+	private List<Player> players;
 	private Drawpile drawpile;
-	
+
 	private static DealCardGame instance;
-	
-	
+
 	public static DealCardGame getInstance() {
-		if (instance==null) {
-			instance=new DealCardGame();
+		if (instance == null) {
+			instance = new DealCardGame();
 		}
 		return instance;
 	}
-	
+
 	private DealCardGame() {
-		// TODO Auto-generated constructor stub
-		drawpile=new Drawpile();
-		
+		players = new ArrayList<>();
+		drawpile = new Drawpile();
 	}
-	
+
 	private void distribute() {
-		for (int i = 0; i < 5; i++) {
-			for (Player player : players) {
-				player.addCard(drawpile.popCard());
-			}
+		int cardsToDistribute = 5 * players.size();
+		for (int i = 0; i < cardsToDistribute; i++) {
+			Player currentPlayer = players.get(i % players.size());
+			currentPlayer.addCard(drawpile.popCard());
 		}
 	}
-	
+
 	public void setPlayerNum(int playerNum) {
 		this.playerNum = playerNum;
-		for (int i = 0; i <playerNum-1; i++) {
-			players.add(new Cpu("Cpu"+i));
+		for (int i = 0; i < playerNum - 1; i++) {
+			players.add(new Cpu("Cpu" + i));
 		}
 		players.add(new Player("Player"));
 		distribute();
 	}
-	
+
 	public List<Player> getPlayers() {
 		return players;
 	}
-	
-	
+
 	public void run() {
-		Scanner scanner=new Scanner(System.in);
-		
-		
+		Scanner scanner = new Scanner(System.in);
+		// 添加游戏逻辑代码
 	}
 
-	
 	public static void main(String[] args) {
-		DealCardGame dealCardGame=new DealCardGame();
-		
+		DealCardGame dealCardGame = DealCardGame.getInstance();
+		// 添加游戏启动逻辑代码
 	}
+
 }
--- DealCards_r000274/DealCards_r000274/src/DealCard/MoneyCard.java
+++ DealCards_r000274/DealCards_r000274/src/DealCard/MoneyCard.java
@@ -1,16 +1,13 @@
 package DealCard;
 
-public class MoneyCard extends Card{
+public class MoneyCard extends Card {
 
-	public MoneyCard( int value, int maxNum) {
+	public MoneyCard(int value, int maxNum) {
 		super(null, value, maxNum);
-		// TODO Auto-generated constructor stub
 	}
-	
+
 	@Override
 	public String imageString() {
-		// TODO Auto-generated method stub
-		return "MoneyCards/"+value+".jpg";
+		return "MoneyCards/" + value + ".jpg";
 	}
-
 }
