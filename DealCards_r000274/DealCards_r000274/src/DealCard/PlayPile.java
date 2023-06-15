563df6c61a79e4db9eaf2fc5f317311c4d41b509
caolangcheng <caolangcheng@gmail.com>
2023-06-15 10:38:22 +0800

    y

--- DealCards_r000274/DealCards_r000274/src/DealCard/PlayPile.java
+++ DealCards_r000274/DealCards_r000274/src/DealCard/PlayPile.java
@@ -3,20 +3,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PlayPile {
-
-    private List<Card> actionCards=new ArrayList<>();
-
-    public void addAction(Card card) {
-        if (card instanceof ActionCard||card instanceof RentCard) {
-            actionCards.add(card);
-        }
-    }
-
-    public void remove(Card card) {
-        actionCards.remove(card);
-    }
-
-
-
+	
+	private List<Card> actionCards=new ArrayList<>();
+	
+	public void addAction(Card card) {
+		if (card instanceof ActionCard||card instanceof RentCard) {
+			actionCards.add(card);
+		}
+	}
+	
+	public void remove(Card card) {
+		actionCards.remove(card);
+	}
+	
+	
+	public List<Card> getActionCards() {
+		return actionCards;
+	}
+	
 
 }
